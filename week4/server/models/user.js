7867622e1c5df99cd25429f39d625007a32b2ee7
stephanieanahi <stephanie.guzman@bryanuniversity.edu>
2022-12-14 20:34:08 -0600

    added auth

---
+++ week4/server/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const userSchema = new Schema({
+
+    
+})
+
+
+
+
+
+
+
+
+
+module.exports = mongoose.model('users', userSchema)
